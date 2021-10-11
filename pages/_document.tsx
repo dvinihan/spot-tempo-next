dbe51a1a20456a5e6ed30a2bc8f4cb950c73ded8
Daniel Vinihan <daniel.vinihan@gmail.com>
2021-10-11 12:05:33 -0500

    fix PWA stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="manifest" href="manifest.json" />
           <meta name="theme-color" content={BACKGROUND_COLOR} />
         </Head>
